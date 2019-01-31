dffc48667dcf5e09a49309900bec6a4879b54a7d
Alex <alexka2025@gmail.com>
2019-01-31 04:35:35 +0000

    Can't save stock - user must exist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
+    
+    if params[:symbol] == ""
+      @nothing = "You didn't enter a symbol"
+    elsif 
+      if params[:symbol]
+        begin
+          @stock = StockQuote::Stock.quote(params[:symbol])
+        rescue
+          @error = "The symbol does not exist"
+        end
+      end
+    end
+    
   end
   
   def about
